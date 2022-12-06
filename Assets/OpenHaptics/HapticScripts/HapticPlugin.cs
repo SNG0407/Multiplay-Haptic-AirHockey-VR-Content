28af35f7b17681e741e9fffb49a4d3b2e1331ce8
SNG0407 <63523334+SNG0407@users.noreply.github.com>
2022-12-06 12:23:16 +0900

    openhaptic ReDownloaded

--- Assets/OpenHaptics/HapticScripts/HapticPlugin.cs
+++ Assets/OpenHaptics/HapticScripts/HapticPlugin.cs
@@ -258,21 +258,7 @@ public class HapticPlugin : MonoBehaviour  {
 		shape_render(configName, M); 
 	}
 		
-	public void tryToInlitailizeHaptic()
-    {
-		bool IsHapticConnected;
-		IsHapticConnected = initializeHapticDevice();
-        if (IsHapticConnected)
-        {
-			Debug.Log("Haptic Connected");
-        }
-        else
-        {
-			Debug.Log("Haptic is Not Connected");
 
-		}
-
-	}
 
 	bool initializeHapticDevice()
 	{
