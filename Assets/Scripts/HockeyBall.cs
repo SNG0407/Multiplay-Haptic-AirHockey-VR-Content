0dfe19d6f33dbb89dfb27888cc945819ee80bc14
SNG0407 <63523334+SNG0407@users.noreply.github.com>
2022-12-09 15:48:08 +0900

    ㅇㅇ

---
+++ Assets/Scripts/HockeyBall.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HockeyBall : MonoBehaviour
+{
+    public Text score1;
+    public Text score2;
+    private int getScore1 = 0;
+    private int getScore2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score1 = GameObject.Find("Score1").GetComponent<Text>();
+        score2 = GameObject.Find("Score2").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScoreText();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject ball;
+        if (collision.collider.gameObject.CompareTag("Goal"))
+        {
+            getScore1++;
+            Debug.Log("Goal");
+            Destroy(gameObject);
+            ball = Instantiate(gameObject);
+            ball.transform.position = new Vector3(0, 11, -10);
+        }
+        //Tag를 골대별로 만들어서 점수 올리면 될 것 같습니다.
+        else if (collision.collider.gameObject.CompareTag("Goal2"))
+        {
+            getScore2++;
+            Debug.Log("Goal");
+            Destroy(gameObject);
+            ball = Instantiate(gameObject);
+            ball.transform.position = new Vector3(0, 11, -10);
+        }
+
+    }
+
+    void SetScoreText()
+    {
+        score1.text = getScore1.ToString();
+        score2.text = getScore2.ToString();
+    }
+}
