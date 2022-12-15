46a7ee73cc24ea986f213c8658c889e63d553cda
SNG0407 <63523334+SNG0407@users.noreply.github.com>
2022-12-15 16:15:59 +0900

    퍽 콜리전에서 트리거로 변경

--- Assets/Scripts/PuckNetworkManager.cs
+++ Assets/Scripts/PuckNetworkManager.cs
@@ -20,6 +20,19 @@ public class PuckNetworkManager : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Collision_Table1" && NetworkManager.Instance.PlayerName == "Player1")
+        {
+            Debug.Log("Player1 owns the puck");
+            ChangeOwnerShip();
+        }
+        if (other.gameObject.name == "Collision_Table2" && NetworkManager.Instance.PlayerName == "Player2")
+        {
+            Debug.Log("Player2 owns the puck");
+            ChangeOwnerShip();
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name== "Collision_Table1" && NetworkManager.Instance.PlayerName == "Player1")
