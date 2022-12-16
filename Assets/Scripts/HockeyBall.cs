0553371d261cc44ec8e793b8497083af23cb9a09
SNG0407 <63523334+SNG0407@users.noreply.github.com>
2022-12-16 11:28:53 +0900

    UI 싱크 맞게 고침..

--- Assets/Scripts/HockeyBall.cs
+++ Assets/Scripts/HockeyBall.cs
@@ -49,29 +49,34 @@ public class HockeyBall : MonoBehaviour
     {
             if (other.gameObject.CompareTag("Goal"))
             {
-                getScore1++;
+                //getScore1++;
                 Debug.Log("Goal 1");
                 //Destroy(gameObject);
                 //ball = Instantiate(gameObject);
                 if (photonView.IsMine)
                 {
+                    photonView.RPC("getScroe1", RpcTarget.All);
                     ball.transform.position = new Vector3(-0.04f, 1.06f, 2.2f); //Player2's ball
                     ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                    photonView.RPC("SetScoreText", RpcTarget.All);
                 }
             }
             //Tag를 골대별로 만들어서 점수 올리면 될 것 같습니다.
             else if (other.gameObject.CompareTag("Goal2"))
             {
-                getScore2++;
+                //getScore2++;
                 Debug.Log("Goal 2");
                 if (photonView.IsMine)
                 {
+                    photonView.RPC("getScroe2", RpcTarget.All);
                     ball.transform.position = new Vector3(-0.04f, 1.06f, -0.5f);
                     ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                    photonView.RPC("SetScoreText", RpcTarget.All);
                 }
 
             }
-            SetScoreText();
+        //SetScoreText();
+        //photonView.RPC("SetScoreText", RpcTarget.All);
     }
 
     private void OnTriggerExit(Collider other)
@@ -87,24 +92,47 @@ public class HockeyBall : MonoBehaviour
             
     }
 
+    [PunRPC]
+    void getScroe1()
+    {
+        getScore1 = getScore1 + 1;
+    }
+
+    [PunRPC]
+    void getScroe2()
+    {
+        getScore2 = getScore2 + 1;
+    }
+
+    [PunRPC]
+    void getScroe(int score)
+    {
+        score++;
+    }
+
+    [PunRPC]
     void SetScoreText()
     {
+        Debug.Log("You're the Master, Update UI");
         Player1_score1.text = getScore1.ToString();
         Player2_score1.text = getScore1.ToString();
         Player1_score2.text = getScore2.ToString();
         Player2_score2.text = getScore2.ToString();
+        
     }
 
     public void ResetBtn()
     {
         getScore1 = 0;
         getScore2 = 0;
-        SetScoreText();
+        //SetScoreText();
+        //photonView.RPC("SetScoreText", RpcTarget.All);
         if (photonView.IsMine)
         {
             //ball.gameObject.GetComponent<PuckNetworkManager>().ChangeOwnerShip();
             this.gameObject.transform.position = new Vector3(-0.08f, 1.06f, 0.86f);
             ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            photonView.RPC("SetScoreText", RpcTarget.All);
         }
         else
         {
